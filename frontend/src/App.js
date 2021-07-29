b13057cc6d7f8aa36e9d70053079b442f5d94a05
Ricky Felix <rickyfelix19@outlook.com>
2021-07-29 15:13:51 +1000

    change material ui to reactstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,22 @@
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
 import React from 'react';
-import Material from '@material-ui/core';
+import {
+  Container,
+}
+from '@material-ui/core';
 
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Navbar />
+      <Footer />
+    </Container>
+
   );
 }
 
