3582f0c00d961ffdf702ea2edd2d613b1d267fbd
Ricky Felix <rickyfelix19@outlook.com>
2021-12-01 14:24:33 +1100

    home page finish

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 
 import ProductCards from './components/ProductCards';
 
-import { Container } from '@mui/material';
+import { Box, Container } from '@mui/material';
 
 class Home extends Component {
   render() {
     return (
       <div>
-        <Container style={{ padding: '20px 0px' }}>
+        <Container sx={{ marginTop: '30px', marginBottom: '110px' }}>
           <ProductCards />
         </Container>
       </div>
--- client/src/pages/components/ProductCards.js
+++ client/src/pages/components/ProductCards.js
@@ -7,71 +7,112 @@ import {
   CardMedia,
   CardActions,
   CardContent,
+  Collapse,
   Button,
+  Typography,
   Box
 } from '@mui/material';
 
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import IconButton from '@mui/material/IconButton';
+
+import { grid, flexbox } from '@mui/system';
+
 import CarDatabase from '../../cars';
+
+import { styled } from '@mui/material/styles';
 import '../stylesheets/ProductCards.scss';
 
-class ProductCards extends Component {
-  render() {
-    // if availability true show, if not hide
-    return (
-      <div style={{ marginBottom: '75px' }}>
+const ExpandMore = styled((props) => {
+  // to expand more description
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest
+  })
+}));
+
+// to check if item is available
+
+export default function ProductCards() {
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+  return (
+    <div>
+      <Box
+        sx={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(5, 1fr)',
+          columnGap: 3,
+          rowGap: 2
+        }}
+      >
         {/* mapping */}
         {CarDatabase.cars.map((car, i) => {
           return (
-            <div>
-              <Card>
-                <CardHeader>{car[i]}</CardHeader>
-                <Box>
-                  <CardMedia>
-                    <div>
-                      <img
-                        src={car.images}
-                        alt={car.brand}
-                        width="320px"
-                        height="245px"
-                      />
-                    </div>
-                  </CardMedia>
-                  <CardContent>
-                    <div>
-                      <Typography>
-                        {car.brand}-{car.model}-(y{car.model_year}) <br />
-                      </Typography>
-                    </div>
-                    <div>
-                      <Typography>
-                        mileage: {car.mileage} <br />
-                        fuel: {car.fuel_type} <br />
-                        seats: {car.seats} <br />
-                        price: {car.price_per_day} <br />
-                        {/* availability: {car.availability} */}
-                      </Typography>
-                      <br /> <br />
-                    </div>
-                    <div>
-                      {/* toggle down */}
-                      <Typography>
-                        Description:
-                        <br /> <br />
-                        {car.description}
-                      </Typography>
-                    </div>
-                  </CardContent>
-                  <CardActions>
-                    <Button>Rent this car</Button>
-                  </CardActions>
-                </Box>
-              </Card>
-            </div>
+            <Card
+              sx={{
+                Width: '300px',
+                Height: '500px'
+              }}
+            >
+              <CardHeader>{car[i]}</CardHeader>
+              {/* Image */}
+              <CardMedia component="img" src={car.images} alt={car.brand} />
+
+              {/* Details */}
+              <CardContent>
+                <Typography>
+                  {car.brand} {car.model}
+                  <br />
+                  (y.{car.model_year})
+                </Typography>
+                <Typography>
+                  <br />
+                  mileage: {car.mileage} <br />
+                  fuel: {car.fuel_type} <br />
+                  seats: {car.seats} <br />
+                  price: {car.price_per_day} <br />
+                  {/* availability: {car.availability} */}
+                </Typography>
+                <br />
+
+                {/* Button */}
+                <CardActions
+                  sx={{
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    display: 'flex'
+                  }}
+                >
+                  <Button>Rent This Car</Button>
+                </CardActions>
+
+                {/* Descriptions */}
+                <CardActions>
+                  <ExpandMore
+                    expand={expanded}
+                    onClick={handleExpandClick}
+                    aria-expanded={expanded}
+                    aria-label="show more"
+                  >
+                    <ExpandMoreIcon />
+                  </ExpandMore>
+                </CardActions>
+                <Collapse in={expanded} timeout="auto" unmountOnExit>
+                  <Typography> Description: {car.description}</Typography>
+                </Collapse>
+              </CardContent>
+            </Card>
           );
         })}
-      </div>
-    );
-  }
+      </Box>
+    </div>
+  );
 }
-
-export default ProductCards;
